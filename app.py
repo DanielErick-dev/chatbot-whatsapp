a5ec3195bf478d6fed27308db3ce6086e384b1ff
Daniel Erick <DanielErick.estudante@gmail.com>
2025-07-16 15:38:06 -0300

    feat(app) add agent response

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI, Request
+from chains import get_conversational_rag_chain
 
 app = FastAPI()
+
+conversational_rag_chain = get_conversational_rag_chain()
 
 
 @app.post("/webhook/{event_type}")
 async def webhook(request: Request, event_type: str):
     webhook_data = await request.json()
     print(f"Evento recebido: {event_type}")
-    print(webhook_data)  # Debug
+    print(webhook_data)
 
     try:
         data = webhook_data.get("data", {})
@@ -18,18 +21,19 @@ async def webhook(request: Request, event_type: str):
             return {"status": "ignored"}
 
         message_data = data.get("message", {})
-        message_text = message_data.get("conversation") or message_data.get("extendedTextMessage", {}).get("text")
-
+        message_text = message_data.get("conversation") or \
+            message_data.get("extendedTextMessage", {}).get("text")
+        print('recebendo mensagem')
         if chat_id and message_text:
             number = chat_id.split("@")[0]
-
-            reply_text = f"Oi, tudo bem?\nVocê me mandou a seguinte mensagem:\n\n'{message_text}'"
-
+            ai_response = conversational_rag_chain.invoke(
+                input={'input': message_text},
+                config={'configurable': {'session_id': chat_id}}
+            )['answer']
             from evolution_api import send_whatsapp_message
-
             send_whatsapp_message(
                 number=number,
-                text=reply_text
+                text=ai_response
             )
             print(f"Resposta enviada para {number}")
 
